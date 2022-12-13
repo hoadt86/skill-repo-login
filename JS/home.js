99423aa6073f5173e6f82728b9dc9c9dc9604f05
hoadt <hoadt@alobridge.com>
2022-12-13 15:30:20 +0700

    validate login form/redirect to homepage after0.5s/display login user in homepage

---
+++ JS/home.js
@@ -0,0 +1,4 @@
+// Lấy value đã lưu từ sessionStorage rồi hiển thị 
+var data = sessionStorage.getItem("mail");
+console.log(data);
+document.write(data);
--- JS/login.js
+++ JS/login.js
@@ -1,13 +1,27 @@
 function loginByUserPassword() {
-    // validate email
+
+    // Fake sẵn 1 màng các tài khoản user
+    let user = [
+        { mail: 'Honghanh@gmail.com', password: '11111111' },
+        { mail: 'doccocaubai@gmail.com', password: '22222222' },
+        { mail: 'marukochan@gmail.com', password: '33333333' }
+
+    ];
+
     var inputEmail = document.getElementById("email").value;
     var checkEmailFrom = document.getElementById("email").value.indexOf('@');
-    console.log(inputEmail);
+    var inputPassword = document.getElementById("password").value;
 
+    // Lưu giá trị inputUser vào sesionStorage
+    sessionStorage.setItem('mail', inputEmail);
+
+    // validate email
+        //1. Chưa nhập email
     if (inputEmail == null || inputEmail == '') {
         document.getElementById("email").style.border = '1px solid red';
         document.getElementById("messageErrorEmail").innerHTML = "Chưa nhập email";
 
+        //2.Email không đúng định dạng
     } else if (checkEmailFrom == -1) {
         document.getElementById("email").style.border = '1px solid red';
         document.getElementById("messageErrorEmail").innerHTML = "Email không đúng định dạng";
@@ -18,14 +32,13 @@ function loginByUserPassword() {
     }
 
     //validate password
-    var inputPassword = document.getElementById("password").value;
-    console.log(inputPassword);
-    console.log(inputPassword.length);
 
+        //3.Chưa nhập Password
     if (inputPassword == null || inputPassword == '') {
         document.getElementById("password").style.border = '1px solid red';
         document.getElementById("messageErrorPassword").innerHTML = "Chưa nhập Password";
 
+        //4. Password không được nhập quá 20 ký tự
     } else if (inputPassword.length > 20) {
         document.getElementById("password").style.border = '1px solid red';
         document.getElementById("messageErrorPassword").innerHTML = "Password không được nhập quá 20 ký tự";
@@ -36,26 +49,22 @@ function loginByUserPassword() {
     }
 
 
-    //Check email pa
-    let user = [
-        { mail: 'Honghanh@gmail.com', password: '11111111' },
-        { mail: 'doccocaubai@gmail.com', password: '22222222' },
-        { mail: 'marukochan@gmail.com', password: '33333333' }
+    //Check email password is correct?
+    if ((document.getElementById("messageErrorEmail").style.display == "none") && (document.getElementById("messageErrorPassword").style.display == "none")) {
+        for (i = 0; i < user.length; i++) {
+                //email và password đúng
+            if ((inputEmail == user[i].mail) && (inputPassword == user[i].password)) {
+                document.getElementById("messageErrorEmailAndPassword").style.display = "none";
+                console.log(document.getElementById("messageErrorEmailAndPassword").style.display);
+                //sau 0.5s redirect sang trang home
+                setTimeout(function () {
+                    window.location = '../html/home.html';
+                }, 500);
 
-    ];
-
-
-    var inputEmail = document.getElementById("email").value;
-    var inputPassword = document.getElementById("password").value;
-    for (i = 0; i < user.length; i++) {
-        if ((inputEmail == user[i].mail) && (inputPassword == user[i].password)) {
-            console.log("oooooooooooooo")
-            document.getElementById("messageErrorEmailAndPassword").style.display = "none";
-            alert("Bạn đã đăng nhập thành công")
-        } else {
-            console.log("aaaaaaaaaaaaaa");
-            document.getElementById("messageErrorEmailAndPassword").style.display = "block";
-            document.getElementById("messageErrorEmailAndPassword").innerHTML = "Thông tin Email hoặc Password chưa đúng";
+                //5. Thông tin Email hoặc Password chưa đúng
+            } else {
+                document.getElementById("messageErrorEmailAndPassword").innerHTML = "Thông tin Email hoặc Password chưa đúng";
+            }
         }
     }
 }
