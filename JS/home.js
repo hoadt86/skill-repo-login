af2b99cdda714232aafae87bd9b0bceeca1c1b7d
hoadt86 <118412638+hoadt86@users.noreply.github.com>
2023-04-12 16:28:34 +0700

    Update home.js
    thêm text

--- JS/home.js
+++ JS/home.js
@@ -1,4 +1,4 @@
-// Lấy value đã lưu từ sessionStorage rồi hiển thị 
+// Lấy value đã lưu từ sessionStorage rồi hiển thị ra màn hình Home
 let data = sessionStorage.getItem("mail");
 console.log(data);
 document.write("Hello "+ data);
